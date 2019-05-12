377e2670a3c357e61e404a5b7f624d8f16e063fd
akhil.a <akhil.a@endurance.com>
2019-05-12 21:08:07 +0530

    created componentes for education

---
+++ src/components/Education.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Education extends React.Component {
+    render() {
+        return (
+            <div id="Education">
+                <h2>Education</h2>
+                <b>Masters in Engineering, Computer Science</b> Cornell University<br/>
+                <b>Bachelors in Engineering, Computer Science</b> M.S Ramaiah Institute of Technology
+            </div>
+        )
+    }
+}
+
+export default Education
